9403d1cfdd08c720dd30350786ada0c7454abbb5
Jakub Waszkiewicz <qbazilla@hotmail.com>
2020-05-03 20:52:11 +0200

    add separate file for woj dicts

---
+++ voivodes.py
@@ -0,0 +1,61 @@
+def get_id_dict():
+    return {
+        'śląskie': 0,
+        'opolskie': 1,
+        'wielkopolskie': 2,
+        'zachodniopomorskie': 3,
+        'świętokrzyskie': 4,
+        'kujawsko-pomorskie': 5,
+        'podlaskie': 6,
+        'dolnośląskie': 7,
+        'podkarpackie': 8,
+        'małopolskie': 9,
+        'pomorskie': 10,
+        'warmińsko-mazurskie': 11,
+        'łódzkie': 12,
+        'mazowieckie': 13,
+        'lubelskie': 14,
+        'lubuskie': 15
+    }
+
+
+def get_lat_dict():
+    return {
+        'śląskie': 0,
+        'opolskie': 1,
+        'wielkopolskie': 51.20,
+        'zachodniopomorskie': 3,
+        'świętokrzyskie': 4,
+        'kujawsko-pomorskie': 5,
+        'podlaskie': 6,
+        'dolnośląskie': 7,
+        'podkarpackie': 8,
+        'małopolskie': 9,
+        'pomorskie': 10,
+        'warmińsko-mazurskie': 11,
+        'łódzkie': 12,
+        'mazowieckie': 13,
+        'lubelskie': 14,
+        'lubuskie': 15
+    }
+
+
+def get_lon_dict():
+    return {
+        'śląskie': 0,
+        'opolskie': 1,
+        'wielkopolskie': 17.14,
+        'zachodniopomorskie': 3,
+        'świętokrzyskie': 4,
+        'kujawsko-pomorskie': 5,
+        'podlaskie': 6,
+        'dolnośląskie': 7,
+        'podkarpackie': 8,
+        'małopolskie': 9,
+        'pomorskie': 10,
+        'warmińsko-mazurskie': 11,
+        'łódzkie': 12,
+        'mazowieckie': 13,
+        'lubelskie': 14,
+        'lubuskie': 15
+    }
