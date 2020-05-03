979f3113c5ce6ef9136a6454ebab176d8d302c97
Jakub Waszkiewicz <qbazilla@hotmail.com>
2020-05-03 21:07:02 +0200

    add almost proper georgraphic coordinates, have to change seconds into frac of degrees

--- voivodes.py
+++ voivodes.py
@@ -21,41 +21,41 @@ def get_id_dict():
 
 def get_lat_dict():
     return {
-        'śląskie': 0,
-        'opolskie': 1,
+        'śląskie': 50.20,
+        'opolskie': 50.38,
         'wielkopolskie': 51.20,
-        'zachodniopomorskie': 3,
-        'świętokrzyskie': 4,
-        'kujawsko-pomorskie': 5,
-        'podlaskie': 6,
-        'dolnośląskie': 7,
-        'podkarpackie': 8,
-        'małopolskie': 9,
-        'pomorskie': 10,
-        'warmińsko-mazurskie': 11,
-        'łódzkie': 12,
-        'mazowieckie': 13,
-        'lubelskie': 14,
-        'lubuskie': 15
+        'zachodniopomorskie': 53.36,
+        'świętokrzyskie': 50.45,
+        'kujawsko-pomorskie': 53.04,
+        'podlaskie': 53.16,
+        'dolnośląskie': 51.05,
+        'podkarpackie': 49.57,
+        'małopolskie': 49.51,
+        'pomorskie': 54.11,
+        'warmińsko-mazurskie': 53.51,
+        'łódzkie': 51.36,
+        'mazowieckie': 52.2,
+        'lubelskie': 51.13,
+        'lubuskie': 52.11
     }
 
 
 def get_lon_dict():
     return {
-        'śląskie': 0,
-        'opolskie': 1,
+        'śląskie': 19,
+        'opolskie': 17.54,
         'wielkopolskie': 17.14,
-        'zachodniopomorskie': 3,
-        'świętokrzyskie': 4,
-        'kujawsko-pomorskie': 5,
-        'podlaskie': 6,
-        'dolnośląskie': 7,
-        'podkarpackie': 8,
-        'małopolskie': 9,
-        'pomorskie': 10,
-        'warmińsko-mazurskie': 11,
-        'łódzkie': 12,
-        'mazowieckie': 13,
-        'lubelskie': 14,
-        'lubuskie': 15
+        'zachodniopomorskie': 15.32,
+        'świętokrzyskie': 20.46,
+        'kujawsko-pomorskie': 18.29,
+        'podlaskie': 22.56,
+        'dolnośląskie': 16.23,
+        'podkarpackie': 22.1,
+        'małopolskie': 20.16,
+        'pomorskie': 18,
+        'warmińsko-mazurskie': 20.49,
+        'łódzkie': 19.25,
+        'mazowieckie': 21.06,
+        'lubelskie': 22.54,
+        'lubuskie': 15.2
     }
